f2d8b75fe3ae14202099d5d5180341a7327f888a
denysnando <denysnando@gmail.com>
2023-10-20 15:08:22 -0300

    Update link_checker_worker_spec.rb

--- spec/workers/link_checker_worker_spec.rb
+++ spec/workers/link_checker_worker_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe LinkCheckerWorker, type: :worker do
   it 'performs the worker' do
     VCR.use_cassette('google_links') do
-      scrape = create(:scrape) # Supondo que você tenha uma factory para criar instâncias de Scrape
+      scrape = create(:scrape) 
       allow(Scrape).to receive(:find).with(scrape.id).and_return(scrape)
 
       expect(LinkChecker).to receive(:call).with(scrape:)
